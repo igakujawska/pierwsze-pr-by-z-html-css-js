26f3d8fb01df2112950083b2d277d0170db9fd74
iga <igakuj1@gmail.com>
2026-07-28 11:12:23 +0200

    added header

--- first-angular-app/src/app/app.ts
+++ first-angular-app/src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header.component';
 
 @Component({
   selector: 'app-root',
--- first-angular-app/src/app/header.component.ts
+++ first-angular-app/src/app/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-header',
-    
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css'
 })
 export class HeaderComponent {} //header to id, component to typ klasy
