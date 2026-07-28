a0df72af34eb4a41c6a0a2a3fb5ed5877af0d20b
iga <igakuj1@gmail.com>
2026-07-28 13:17:21 +0200

    property binding outputting computed values

--- first-angular-app/src/app/user/user.ts
+++ first-angular-app/src/app/user/user.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length) //wylosowanie z listy pojedynczego usera
 
 @Component({
   selector: 'app-user',
@@ -6,4 +9,6 @@ import { Component } from '@angular/core';
   templateUrl: './user.html',
   styleUrl: './user.css',
 })
-export class User {}
+export class User {
+  selectedUser = DUMMY_USERS[randomIndex]
+}
