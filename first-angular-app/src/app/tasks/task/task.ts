1af6e73cc5ac1f40be1fa4b6981398d5ad9550fb
iga <igakuj1@gmail.com>
2026-07-30 17:03:09 +0200

    outputting task data in the task component

--- first-angular-app/src/app/tasks/task/task.ts
+++ first-angular-app/src/app/tasks/task/task.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+interface Tassk{
+  id: string;
+  userId: string;
+  title: string;
+  summary: string;
+  dueDate: string;
+}
 
 @Component({
   selector: 'app-task',
@@ -6,4 +14,6 @@ import { Component } from '@angular/core';
   templateUrl: './task.html',
   styleUrl: './task.css',
 })
-export class Task {}
+export class Task {
+  @Input({required: true}) task!: Tassk;
+}
